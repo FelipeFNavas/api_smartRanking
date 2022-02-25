1f64db40367e29e90021563d42bc70b72a6fd0c8
Felipe Navas <felipefnavas@gmail.com>
2022-02-25 16:25:56 -0300

    feat: criacao dos arquivos de categorias

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,14 @@
 import { Module } from '@nestjs/common';
 import { JogadoresModule } from './jogadores/jogadores.module';
 import { MongooseModule } from '@nestjs/mongoose';
+import { CategoriasModule } from './categorias/categorias.module';
 
 @Module({
   imports: [
     MongooseModule.forRoot('mongodb+srv://admin:BJBFpNbCUAGsc7G2@cluster0.5aop2.mongodb.net', {dbName: 'myFirstDatabase'}),
     // { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true, useFindAndModify: true}),
-    JogadoresModule
+    JogadoresModule,
+    CategoriasModule
   ],
   controllers: [],
   providers: [],
